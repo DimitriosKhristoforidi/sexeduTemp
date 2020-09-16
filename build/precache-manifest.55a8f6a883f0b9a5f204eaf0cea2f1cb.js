1076c5681fec8c5e287c9e273f7d9ec59671444a
dimitiros <dimitrios.khristoforidi@gmail.com>
2020-09-16 21:13:21 +0600

    first-build

---
+++ build/precache-manifest.55a8f6a883f0b9a5f204eaf0cea2f1cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af177f93e083694a681adfaa8f3f4547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95fcf9a4d668c3e4b679",
+    "url": "/static/css/2.6c9b7e7c.chunk.css"
+  },
+  {
+    "revision": "2b14449f45cca37f5ffd",
+    "url": "/static/css/main.f4ffbb3c.chunk.css"
+  },
+  {
+    "revision": "95fcf9a4d668c3e4b679",
+    "url": "/static/js/2.bb9178f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bb9178f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b14449f45cca37f5ffd",
+    "url": "/static/js/main.edeb2af4.chunk.js"
+  },
+  {
+    "revision": "eb6d44b65cba17836cda",
+    "url": "/static/js/runtime-main.aa641440.js"
+  },
+  {
+    "revision": "184ba927bf2d644cc6fba06d46c2967a",
+    "url": "/static/media/bg.184ba927.png"
+  },
+  {
+    "revision": "7309d0cec348e1b651015361d2e4955c",
+    "url": "/static/media/main.7309d0ce.jpg"
+  },
+  {
+    "revision": "43d4da5b5b673d7868cb8855fc78144f",
+    "url": "/static/media/parentsBlogBg.43d4da5b.png"
+  },
+  {
+    "revision": "34be44d34d8139a89722d907ceacaa3b",
+    "url": "/static/media/parentsBlogImg.34be44d3.png"
+  },
+  {
+    "revision": "efb3312683cc315f0a118de9234f0c68",
+    "url": "/static/media/sexBlogBg.efb33126.png"
+  },
+  {
+    "revision": "0342b8ba53fbbe4f9339937d3ccb04c9",
+    "url": "/static/media/sexBlogImg.0342b8ba.png"
+  },
+  {
+    "revision": "8c2dfeb4348cc5a5f318cab2e40a73e3",
+    "url": "/static/media/storiesBlogBg.8c2dfeb4.png"
+  }
+]);
